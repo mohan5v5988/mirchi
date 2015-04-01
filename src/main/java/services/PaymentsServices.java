0d00e5bd992c79e3002e5ccd369e8d2adbeb34b9
mohan5v5988 <mohan5v5988@gmail.com>
2015-04-01 18:11:14 -0400

    FE fully working for payments

--- src/main/java/services/PaymentsServices.java
+++ src/main/java/services/PaymentsServices.java
@@ -17,6 +17,7 @@ import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.server.mvc.Viewable;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import model.Due;
@@ -27,6 +28,38 @@ import command.payments.*;
 public class PaymentsServices {
 	ObjectMapper mapper = new ObjectMapper();
 	
+	@GET
+	@Path("metadata")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response getSongMeta() {
+		Payments p = new Payments();
+		try {
+			@SuppressWarnings("unchecked")
+			HashMap songHM = mapper.convertValue(p, HashMap.class);
+			return Response.status(200).entity(mapper.writeValueAsString(songHM)).build();
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return Response.status(500).build();
+	}
+	
+	// get all
+	@GET
+//	@Produces({ MediaType.APPLICATION_JSON , MediaType.TEXT_PLAIN})
+	public Response getTransactions() {
+		ArrayList<Payments> arr = new ArrayList<Payments>();
+		String paymentString = null;
+		ListAllPayments command = new ListAllPayments();
+		arr = command.execute();
+		try {
+			paymentString = mapper.writeValueAsString(arr);
+		} catch(Exception e) {
+			e.printStackTrace();
+			return Response.status(Response.Status.BAD_REQUEST).entity("Please check the values").build();
+		}
+		return Response.status(Response.Status.OK).entity(paymentString).build();
+	}
+	
 	// get by nid, date, id and "nid and date".
 	@GET
 	@Path("/get")
@@ -35,71 +68,73 @@ public class PaymentsServices {
 							@DefaultValue("0") @QueryParam("id") int id,
 							@DefaultValue("1963-12-22") @QueryParam("date") String date) {
 		ArrayList<Payments> arr = new ArrayList<Payments>();
-		HashMap<String, Object> hm = new HashMap<String, Object>();
-		if(nid.equals("nothing") && date.equals("1963-12-22") && id == 0) {
-			return Response.status(Response.Status.BAD_REQUEST).entity("Please enter any value to search.").build();
-		} else if(nid.equals("nothing") ^ date.equals("1963-12-22")) {
-			if(nid.equals("nothing")) {
-				GetPaymentsByDateCommand command = new GetPaymentsByDateCommand();
-				hm.put("Payments", command.execute(date));
-			} else if(date.equals("1963-12-22")){
-				GetPaymentsByNidCommand command = new GetPaymentsByNidCommand();
-				hm.put("Payments", command.execute(nid));
-			}
-		} else if(id == 0) {
-			GetPaymentsByNidandDateCommand command = new GetPaymentsByNidandDateCommand();
-			hm.put("Payments", command.execute(nid, date));
-		} else {
-			GetPaymentsByIdCommand command = new GetPaymentsByIdCommand();
-			arr.add(command.execute(id));
-			hm.put("Payments", arr);
-		}
-		return Response.ok(new Viewable("/payments/DisplayP.jsp", hm)).build();
-		
-		
-		
+		String paymentString = null;
+//		HashMap<String, Object> hm = new HashMap<String, Object>();
 //		if(nid.equals("nothing") && date.equals("1963-12-22") && id == 0) {
 //			return Response.status(Response.Status.BAD_REQUEST).entity("Please enter any value to search.").build();
 //		} else if(nid.equals("nothing") ^ date.equals("1963-12-22")) {
 //			if(nid.equals("nothing")) {
 //				GetPaymentsByDateCommand command = new GetPaymentsByDateCommand();
-//				arr = command.execute(date);
-//				try {
-//					paymentString = mapper.writeValueAsString(arr);
-//				} catch(Exception e) {
-//					e.printStackTrace();
-//					return Response.status(Response.Status.BAD_REQUEST).entity("Please check the values").build();
-//				}
+//				hm.put("Payments", command.execute(date));
 //			} else if(date.equals("1963-12-22")){
 //				GetPaymentsByNidCommand command = new GetPaymentsByNidCommand();
-//				arr = command.execute(nid);
-//				try {
-//					paymentString = mapper.writeValueAsString(arr);
-//				} catch(Exception e) {
-//					e.printStackTrace();
-//					return Response.status(Response.Status.BAD_REQUEST).entity("Please check the values").build();
-//				}
+//				hm.put("Payments", command.execute(nid));
 //			}
 //		} else if(id == 0) {
 //			GetPaymentsByNidandDateCommand command = new GetPaymentsByNidandDateCommand();
-//			arr = command.execute(nid, date);
-//			try {
-//				paymentString = mapper.writeValueAsString(arr);
-//			} catch(Exception e) {
-//				e.printStackTrace();
-//				return Response.status(Response.Status.BAD_REQUEST).entity("Please check the values").build();
-//			}
+//			hm.put("Payments", command.execute(nid, date));
 //		} else {
 //			GetPaymentsByIdCommand command = new GetPaymentsByIdCommand();
-//			Payments a = command.execute(id);
-//			try {
-//				paymentString = mapper.writeValueAsString(a);
-//			} catch(Exception e) {
-//				e.printStackTrace();
-//				return Response.status(Response.Status.BAD_REQUEST).entity("Please check the values").build();
-//			}
+//			arr.add(command.execute(id));
+//			hm.put("Payments", arr);
 //		}
-//		return Response.status(Response.Status.OK).entity(paymentString).build();
+//		return Response.ok(new Viewable("/payments/DisplayP.jsp", hm)).build();
+		
+		
+		
+		if(nid.equals("nothing") && date.equals("1963-12-22") && id == 0) {
+			return Response.status(Response.Status.BAD_REQUEST).entity("Please enter any value to search.").build();
+		} else if(nid.equals("nothing") ^ date.equals("1963-12-22")) {
+			if(nid.equals("nothing")) {
+				GetPaymentsByDateCommand command = new GetPaymentsByDateCommand();
+				arr = command.execute(date);
+				try {
+					paymentString = mapper.writeValueAsString(arr);
+				} catch(Exception e) {
+					e.printStackTrace();
+					return Response.status(Response.Status.BAD_REQUEST).entity("Please check the values").build();
+				}
+			} else if(date.equals("1963-12-22")){
+				GetPaymentsByNidCommand command = new GetPaymentsByNidCommand();
+				arr = command.execute(nid);
+				try {
+					paymentString = mapper.writeValueAsString(arr);
+				} catch(Exception e) {
+					e.printStackTrace();
+					return Response.status(Response.Status.BAD_REQUEST).entity("Please check the values").build();
+				}
+			}
+		} else if(id == 0) {
+			GetPaymentsByNidandDateCommand command = new GetPaymentsByNidandDateCommand();
+			arr = command.execute(nid, date);
+			try {
+				paymentString = mapper.writeValueAsString(arr);
+			} catch(Exception e) {
+				e.printStackTrace();
+				return Response.status(Response.Status.BAD_REQUEST).entity("Please check the values").build();
+			}
+		} else {
+			GetPaymentsByIdCommand command = new GetPaymentsByIdCommand();
+			Payments a = command.execute(id);
+			arr.add(a);
+			try {
+				paymentString = mapper.writeValueAsString(arr);
+			} catch(Exception e) {
+				e.printStackTrace();
+				return Response.status(Response.Status.BAD_REQUEST).entity("Please check the values").build();
+			}
+		}
+		return Response.status(Response.Status.OK).entity(paymentString).build();
 	}
 	
 	// get with two dates and nid.
@@ -108,34 +143,34 @@ public class PaymentsServices {
 //	@Produces({ MediaType.APPLICATION_JSON })
 	public Response getPaymentByDatesOrNid(@PathParam("date1") String date1,@PathParam("date2") String date2,
 			@DefaultValue("nothing") @QueryParam("nid") String nid) {
-		HashMap<String, Object> hm = new HashMap<String, Object>();
-		if(nid.equals("nothing")) {
-			GetPaymentsBetweenDatesCommand command = new GetPaymentsBetweenDatesCommand();
-			hm.put("Payments", command.execute(date1,date2));
-		}else {
-			GetPaymentsBetweenDatesAndNidCommand command = new GetPaymentsBetweenDatesAndNidCommand();
-			hm.put("Payments", command.execute(date1,date2,nid));
-		}
-		return Response.ok(new Viewable("/payments/DisplayP.jsp", hm)).build();
+//		HashMap<String, Object> hm = new HashMap<String, Object>();
 //		if(nid.equals("nothing")) {
 //			GetPaymentsBetweenDatesCommand command = new GetPaymentsBetweenDatesCommand();
-//			String paymentString = null;
-//			try {
-//				paymentString = mapper.writeValueAsString(command.execute(date1,date2));
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//			return Response.status(200).entity(paymentString).build();
+//			hm.put("Payments", command.execute(date1,date2));
 //		}else {
 //			GetPaymentsBetweenDatesAndNidCommand command = new GetPaymentsBetweenDatesAndNidCommand();
-//			String paymentString = null;
-//			try {
-//				paymentString = mapper.writeValueAsString(command.execute(date1,date2,nid));
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//			return Response.status(200).entity(paymentString).build();
+//			hm.put("Payments", command.execute(date1,date2,nid));
 //		}
+//		return Response.ok(new Viewable("/payments/DisplayP.jsp", hm)).build();
+		if(nid.equals("nothing")) {
+			GetPaymentsBetweenDatesCommand command = new GetPaymentsBetweenDatesCommand();
+			String paymentString = null;
+			try {
+				paymentString = mapper.writeValueAsString(command.execute(date1,date2));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return Response.status(200).entity(paymentString).build();
+		}else {
+			GetPaymentsBetweenDatesAndNidCommand command = new GetPaymentsBetweenDatesAndNidCommand();
+			String paymentString = null;
+			try {
+				paymentString = mapper.writeValueAsString(command.execute(date1,date2,nid));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return Response.status(200).entity(paymentString).build();
+		}
 	}
 	
 	
